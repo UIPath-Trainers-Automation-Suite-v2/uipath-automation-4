e9ba84f5c935bf333c4b460cd9d877515367aa47
mannj2015 <72755084+mannj2015@users.noreply.github.com>
2020-12-22 14:15:06 -0500

    Now gives info on remote branches for repository such as which they are connected to.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //----------- input goes here ------------------------------
             string filePath = @"C:\Users\mannj\Documents\uipath-automation-4";
+            //string filePath = @"C:\Users\mannj\Documents\GitHub\rev_uipath_wrkspc\MannJames-code";
+            //----------------------------------------------------------
 
             //Return changes in working directory
             using (var repo = new Repository(filePath))
@@ -40,17 +43,44 @@ namespace ConsoleApp1
             }
 
 
-            //git diff {filename} local.patch
             var repo1 = new Repository(filePath);
+
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Repository File Path:");
+            Console.WriteLine(filePath);
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------");
+            //Branch remote info
+            foreach (var item in repo1.Branches)
+            {
+                Console.WriteLine("Remote Name: "+ item.RemoteName);
+                Console.WriteLine("Local connected to remote: " + item.IsTracking);
+                Console.WriteLine("Instance is remote: " + item.IsRemote);
+                Console.WriteLine("Reference: " + item.Reference);
+                Console.WriteLine("Remote branch connected to local (if any): " + item.TrackedBranch);
+                Console.WriteLine("Additional remote branch info: " + item.TrackingDetails);
+                Console.WriteLine("Upstream branch name: " + item.UpstreamBranchCanonicalName);
+                Console.WriteLine("--------------------------------------------------------");
+            }
+
+            Console.WriteLine("--------------------------------------------------------");
+            //git diff {filename} local.patch
+            //Patch info
             foreach (var item in repo1.RetrieveStatus())
             {
 /*                if (item.State == FileStatus.Modified)
                 {*/
-                    var patch = repo1.Diff.Compare<Patch>(new List<string>() { item.FilePath });
-                    Console.WriteLine("~~~~ Patch file ~~~~");
-                    Console.WriteLine(patch.Content);
+                var patch = repo1.Diff.Compare<Patch>(new List<string>() { item.FilePath });
+                Console.WriteLine("~~~~ Patch file ~~~~");
+                Console.WriteLine(patch.Content);
                 //}
             }
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("End of Run");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------------");
+
 
         }
     }
